000c8c0a2da6efcd664d90bf8b27e8a1f25dc6f0
Maddof <majd.azar88@gmail.com>
2023-12-13 21:07:03 +0100

    Created helper function for setting grid and added loadevent to set inital grid

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 const container = document.getElementById("container");
 
-// set grid density by multiplying user input
-// Then loops
+// Sets the inital grid on page load to a 16x16 (16x16=256 grid items)
+document.addEventListener("DOMContentLoaded", setGrid(256));
 
-function changeGridDensity() {
-  let gridDensity = prompt("Write a number", 16);
-  const itemProportion = 100 / gridDensity + "%";
-  gridDensity *= gridDensity;
-  container.innerText = "";
+// Helper function for setting the grid
+
+function setGrid(gridDensity = 256, itemProportion = 6.25 + "%") {
   for (let i = 0; i < gridDensity; i++) {
     const javaDiv = document.createElement("div");
     javaDiv.className = "created-div";
+    container.appendChild(javaDiv);
     javaDiv.setAttribute(
       "style",
       `min-width: ${itemProportion}; min-height: ${itemProportion};`
     );
-
     container.appendChild(javaDiv);
   }
 }
 
-for (let i = 0; i < 256; i++) {
-  const javaDiv = document.createElement("div");
-  javaDiv.className = "created-div";
-  container.appendChild(javaDiv);
+// set grid density by multiplying user input and appending % to use in styling.
+// Nest setGrid() function to pass arguments to it.
+
+function changeGridDensity() {
+  let gridDensity = prompt("Type in a number less then 100 to set your grid");
+  if (
+    gridDensity <= 0 ||
+    gridDensity > 100 ||
+    isNaN(gridDensity) ||
+    gridDensity === null
+  ) {
+    alert("Please pick a number less then 100");
+  } else {
+    const itemProportion = 100 / gridDensity + "%";
+    gridDensity *= gridDensity;
+    container.innerText = "";
+    // Passing the calculations to the helper function.
+    setGrid(gridDensity, itemProportion);
+  }
 }
+
+// Add event delegation to the parent but selecting only the current target that has the created-div class
+// Instead of only mouseover event, user also has to hold ctrlKey to sketch.
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.className === "created-div" && e.ctrlKey) {
     e.target.classList.add("hover-color");
-    console.log(e.target.classList);
   }
 });
