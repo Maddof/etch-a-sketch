73d6e738407e5592f2edfbfad2d9d99a2f786505
Maddof <97884486+Maddof@users.noreply.github.com>
2023-12-13 19:38:22 +0100

    Add change grid via button

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const container = document.getElementById("container");
 
-//container.appendChild(javaDiv);
+// set grid density by multiplying user input
+// Then loops
+
+function changeGridDensity() {
+  let gridDensity = prompt("Write a number", 16);
+  const itemProportion = 100 / gridDensity + "%";
+  gridDensity *= gridDensity;
+  container.innerText = "";
+  for (let i = 0; i < gridDensity; i++) {
+    const javaDiv = document.createElement("div");
+    javaDiv.className = "created-div";
+    javaDiv.setAttribute(
+      "style",
+      `min-width: ${itemProportion}; min-height: ${itemProportion};`
+    );
+
+    container.appendChild(javaDiv);
+  }
+}
 
 for (let i = 0; i < 256; i++) {
   const javaDiv = document.createElement("div");
@@ -9,7 +27,7 @@ for (let i = 0; i < 256; i++) {
 }
 
 container.addEventListener("mouseover", (e) => {
-  if (e.target.className === "created-div") {
+  if (e.target.className === "created-div" && e.ctrlKey) {
     e.target.classList.add("hover-color");
     console.log(e.target.classList);
   }
